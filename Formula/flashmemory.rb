fd122fbc7f68f82f4b18bca3f3b7154d69d2978e
ZetaZeroHub <contact@zzh.app>
2026-04-13 18:54:22 +0800

    release: version 0.4.5

--- Formula/flashmemory.rb
+++ Formula/flashmemory.rb
@@ -11,7 +11,7 @@
 class Flashmemory < Formula
   desc "Cross-language code analysis and semantic search system"
   homepage "https://github.com/ZetaZeroHub/FlashMemory"
-  version "0.4.4"
+  version "0.4.5"
   license "MIT"
 
   on_macos do
